c1fe4f8045190358aa7cba4fbd45952de9507f53
mhlky <mhlky@163.com>
2013-05-08 18:23:12 +0800

    create the first class

---
+++ onlineStudy/src/main/java/com/friend/study/system/model/Test.java
@@ -0,0 +1,18 @@
+package com.friend.study.system.model;
+
+import java.util.*;
+
+import
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lky
+ * Date: 13-5-8
+ * Time: 下午6:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("changed by lky");
+    }
+}
